fb400f91f2625d877cc60ec25ca90d22975bae3d
Jinto Jose <jintoppy@gmail.com>
2017-12-13 12:11:43 +0530

    coming back to form with validation

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ angular.module('myapp')
     .component('app', {
         templateUrl: 'app.component.html',
         controller: function(){
-            this.onSubmit = function(){
-                alert('submitted');
+            this.onSubmit = function(myform){
+                if(myform.$valid){
+                    console.log(this.username);
+                    console.log(this.password);
+                }
+                else{
+                    alert('fix validation errors');
+                }
+                
             };
         }
     });
